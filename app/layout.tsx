3bf450bf1c08c552c4ed6e910aa69b4f032314d7
Zeshanulhaq <zeshanulhaq1655@gmail.com>
2023-09-24 15:32:43 +0500

    final-commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "JS Mastery",
+  description: "JS Mastery Resources",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-black-100 font-poppins">{children}</body>
+    </html>
+  );
+}
